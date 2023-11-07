694ef2e2a0f79a74c4fedd60e518bb1ae8a68a1a
mx-jrk <max.yurkov@gmail.com>
2023-11-07 22:15:17 +0300

    add: task_3 finished

---
+++ src/Practice_18/Task_3/Exception3.java
@@ -0,0 +1,17 @@
+package Practice_18.Task_3;
+
+import java.util.Scanner;
+
+public class Exception3 {
+    public void exceptionDemo() {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter an integer ");
+        String intString = myScanner.next();
+        try {
+            int i = Integer.parseInt(intString);
+            System.out.println(2 / i);
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
