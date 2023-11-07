ebf28a11475fad3bcff5dbe994b82ab27f1d282f
mx-jrk <max.yurkov@gmail.com>
2023-11-07 22:13:33 +0300

    add: task_2 finished

---
+++ src/Practice_18/Task_2/Exception2.java
@@ -0,0 +1,19 @@
+package Practice_18.Task_2;
+
+import java.util.Scanner;
+
+public class Exception2 {
+    public void exceptionDemo() {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter an integer ");
+        String intString = myScanner.next();
+        try {
+            int i = Integer.parseInt(intString);
+            System.out.println(2 / i);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter an integer.");
+        } catch (ArithmeticException e) {
+            System.out.println("Attempted division by zero.");
+        }
+    }
+}
